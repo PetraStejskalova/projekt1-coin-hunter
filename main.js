979f02a7bb31c1bc97cec4b899d9e99bd4a3fd91
Petra Stejskalova <pt.stejskalova@gmail.com>
2022-03-04 20:42:46 +0100

    Coin Hunter movement restriction

--- main.js
+++ main.js
@@ -13,21 +13,47 @@ let boy = document.getElementById('panacek');
 let x = window.innerWidth * 0.5 - 32;
 let y = window.innerHeight * 0.5 - 35;
 
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+let boyHeight = 64;
+let boyWidth = 70;
+
+console.log(windowHeight);
+console.log(windowHeight - boyHeight);
+
 boy.style.left = x + 'px';
 boy.style.top = y + 'px';
 
 function move(event) {
+	console.log(boy.style.left);
 	if (event.key === 'ArrowDown') {
-		y = y + 50;
-		boy.style.top = y + 'px';
+		if (y > windowHeight - boyHeight - 10) {
+			boy.style.top = windowHeight - boyHeight - 10 + 'px';
+		} else {
+			y = y + 10;
+			boy.style.top = y + 'px';
+		}
 	} else if (event.key === 'ArrowUp') {
-		y = y - 50;
-		boy.style.top = y + 'px';
+		if (y < 0) {
+			boy.style.top = 0 + 'px';
+		} else {
+			y = y - 10;
+			boy.style.top = y + 'px';
+		}
 	} else if (event.key === 'ArrowRight') {
-		x = x + 50;
-		boy.style.left = x + 'px';
+		if (x > windowWidth - boyWidth) {
+			boy.style.left = windowWidth - boyWidth + 'px';
+		} else {
+			x = x + 10;
+			boy.style.left = x + 'px';
+		}
 	} else if (event.key === 'ArrowLeft') {
-		x = x - 50;
-		boy.style.left = x + 'px';
+		if (x < 0) {
+			boy.style.left = 0 + 'px';
+		} else {
+			x = x - 10;
+			boy.style.left = x + 'px';
+		}
 	}
 }
